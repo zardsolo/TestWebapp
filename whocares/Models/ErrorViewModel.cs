152472d7b71a8d5606b9b24c4cbe9c2c7036d4f7
zardsolo <104177365+zardsolo@users.noreply.github.com>
2023-08-16 10:52:57 +0930

    Add project files.

---
+++ whocares/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace whocares.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
