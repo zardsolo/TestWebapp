473594341e223cad292609cfb58811a28ad40a23
Gabe Young <Gabe_young@outlook.com>
2023-09-11 13:30:10 +0930

    Completed image upload and & Area account identityforms

---
+++ whocares/Models/ImageModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace puppyPlayDates.Models
+{
+    public class ImageModel
+    {
+        // public string? UserImgUrl { get; set; }
+        [Display(Name = "Upload an image of your pet")]
+        public IFormFile UserImg { get; set; }
+    }
+}
