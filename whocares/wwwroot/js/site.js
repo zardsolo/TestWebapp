3c92ecea29048f2fb76ab416a26ec94159e50950
zardsolo <104177365+zardsolo@users.noreply.github.com>
2023-09-11 14:15:24 +0930

    minor changes, youtube link and a dependency

--- whocares/wwwroot/js/site.js
+++ whocares/wwwroot/js/site.js
@@ -3,6 +3,8 @@
 
 // Write your JavaScript code.
 
+//https://www.youtube.com/watch?v=i7oL_K_FmM8
+
 const dogSelect = document.querySelector("#dogSelect");
 const catSelect = document.querySelector("#catSelect");
 const catDogSelect = document.querySelector("#catDogSelect");
